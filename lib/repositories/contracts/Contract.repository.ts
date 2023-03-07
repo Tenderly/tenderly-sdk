f1478f92e0be788157e6e5834bd0f7947ed287df
Aleksandar Milosavljević <alemilosav@gmail.com>
2023-03-07 12:45:49 +0100

    fix: remove not needed try catch

--- lib/repositories/contracts/Contract.repository.ts
+++ lib/repositories/contracts/Contract.repository.ts
@@ -133,33 +133,28 @@ export class ContractRepository implements Repository<Contract> {
     });
 
   async verify(address: string, verificationRequest: VerificationRequest) {
-    try {
-      const result = await this.api.post(
-        `account/${this.configuration.accountName}/project/${this.configuration.projectName}/contracts`, {
-        config: {
-          optimization_count:
-            verificationRequest.solc.compiler.settings.optimizer.enabled
-              ? verificationRequest.solc.compiler.settings.optimizer.runs
-              : null,
+    const result = await this.api.post(
+      `account/${this.configuration.accountName}/project/${this.configuration.projectName}/contracts`, {
+      config: {
+        optimization_count:
+          verificationRequest.solc.compiler.settings.optimizer.enabled
+            ? verificationRequest.solc.compiler.settings.optimizer.runs
+            : null,
 
-        }, contracts: Object.keys(verificationRequest.solc.sources).map((path: string) => ({
-          contractName: verificationRequest.solc.sources[path].name,
-          source: verificationRequest.solc.sources[path].source,
-          sourcePath: path,
-          networks: {
-            [this.configuration.network]: { address: address, links: {} }
-          },
-          compiler: {
-            name: 'solc',
-            version: verificationRequest.solc.compiler.version,
-          }
-        }))
-      });
+      }, contracts: Object.keys(verificationRequest.solc.sources).map((path: string) => ({
+        contractName: verificationRequest.solc.sources[path].name,
+        source: verificationRequest.solc.sources[path].source,
+        sourcePath: path,
+        networks: {
+          [this.configuration.network]: { address: address, links: {} }
+        },
+        compiler: {
+          name: 'solc',
+          version: verificationRequest.solc.compiler.version,
+        }
+      }))
+    });
 
-      return result;
-    } catch (error) {
-      console.error('Error: ', error);
-      throw error;
-    }
+    return result;
   };
 }
