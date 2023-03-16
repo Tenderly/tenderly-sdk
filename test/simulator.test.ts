a29543c494ee745a4180d6d3b5fcaac1d8ee2c29
Danko Krupljanin <dkrupljanin@gmail.com>
2023-03-16 15:39:19 +0100

    refactor: extend timeout for simulator tests

--- test/simulator.test.ts
+++ test/simulator.test.ts
@@ -1,6 +1,8 @@
 import { Network, Tenderly } from '../lib';
 import { Interface } from 'ethers';
 
+jest.setTimeout(25000);
+
 let tenderly: Tenderly = null;
 
 const counterContract = '0x2e4534ad99d5e7fffc9bbe52df69ba8febeb0057';
@@ -8,41 +10,46 @@ const nullAddress = '0x0000000000000000000000000000000000000000';
 
 const abi = [
   {
-    "inputs": [],
-    "name": "count",
-    "outputs": [{
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: 'count',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
   },
   {
-    "inputs": [],
-    "name": "dec",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [],
+    name: 'dec',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
   },
   {
-    "inputs": [],
-    "name": "get",
-    "outputs":
-      [{
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }],
-    "stateMutability": "view",
-    "type": "function"
-  }, {
-    "inputs": [],
-    "name": "inc",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }];
+    inputs: [],
+    name: 'get',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'inc',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
 
 const abiInterface = new Interface(abi);
 
@@ -67,12 +74,12 @@ test('simulateTransaction works', async () => {
     blockNumber: 12354651,
     override: {
       counterContract: {
-        "storage": {
-          "0x0000000000000000000000000000000000000000000000000000000000000000":
-            "0x0000000000000000000000000000000000000000000000000000000000000009"
-        }
-      }
-    }
+        storage: {
+          '0x0000000000000000000000000000000000000000000000000000000000000000':
+            '0x0000000000000000000000000000000000000000000000000000000000000009',
+        },
+      },
+    },
   });
 
   expect(transaction.timestamp).toBeDefined();
