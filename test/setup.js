786ba44cac1f1e3e17aecd7a4e9c47d0d33f3114
Aleksandar Milosavljević <alemilosav@gmail.com>
2023-04-11 10:54:42 +0200

    fix: revert debug workflow

--- test/setup.js
+++ test/setup.js
@@ -3,9 +3,7 @@ const dotenv = require('dotenv');
 
 module.exports = () => {
   try {
-    console.log('###### ENV before setup: ', process.env.TENDERLY_ACCOUNT_NAME);
     dotenv.config();
-    console.log('###### ENV after setup: ', process.env.TENDERLY_ACCOUNT_NAME);
 
     console.log('Jest setup successfull!');
   } catch (error) {
