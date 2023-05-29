d62f3b50fc078b03a7d62a420a7cf76e79d00928
dule-git <dusank98@gmail.com>
2023-05-29 12:48:46 +0200

    Revert "fix: replaced unexpected_verification_error with bytecode_mismatch_error"
    This reverts commit 4088f70ea7ed8a33301cac341a7511abcde87809.

--- test/contracts.repository.test.ts
+++ test/contracts.repository.test.ts
@@ -1,7 +1,7 @@
 import { Tenderly, Network } from '../lib';
 import { NotFoundError } from '../lib/errors/NotFoundError';
 import { CompilationError } from '../lib/errors/CompilationError';
-import { BytecodeMismatchError } from '../lib/errors/BytecodeMismatchError';
+import { UnexpectedVerificationError } from '../lib/errors/UnexpectedVerificationError';
 
 const counterContractSource = `
 // SPDX-License-Identifier: MIT
@@ -418,8 +418,8 @@ describe('contracts.verify', () => {
         },
       });
     } catch (error) {
-      expect(error instanceof BytecodeMismatchError).toBeTruthy();
-      expect(error.slug).toEqual('bytecode_mismatch_error');
+      expect(error instanceof UnexpectedVerificationError).toBeTruthy();
+      expect(error.slug).toEqual('unexpected_verification_error');
     }
   });
 });
